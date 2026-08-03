cf96f076dd77120238405141ac4c5a9565016a18
João Pedro <k4sstops@outlook.com>
2026-08-03 15:54:12 -0300

    MongoDB

--- core/api/src/main/java/com/ascend/core/api/Constants.java
+++ core/api/src/main/java/com/ascend/core/api/Constants.java
@@ -5,7 +5,7 @@ public final class Constants {
     private Constants() {}
 
     public static final String DATABASE_NAME = "ascend";
-    public static final String MONGO_URI     = "mongodb://admin:M0ng0%40%232026%21xP@151.244.40.248:27017/ascend?authSource=admin";
+    public static final String MONGO_URI     = "";
 
     public static final String REDIS_HOST     = "127.0.0.1";
     public static final int    REDIS_PORT     = 6379;
